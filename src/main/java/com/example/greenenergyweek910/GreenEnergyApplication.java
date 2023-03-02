99c9e24f2ed65c5693e604bdc14609184698f312
Søren <112867530+Ullaabdulla420@users.noreply.github.com>
2023-03-02 22:21:53 +0100

    Logo osv
    Flot sol und so weiter

--- src/main/java/com/example/greenenergyweek910/GreenEnergyApplication.java
+++ src/main/java/com/example/greenenergyweek910/GreenEnergyApplication.java
@@ -2,8 +2,15 @@ package com.example.greenenergyweek910;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.ComboBoxTableCell;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,9 +25,23 @@ public class GreenEnergyApplication extends Application {
 
         stage.setTitle("Green Energy");
         stage.setScene(scene);
+        stage.getIcons().add(new Image("C:\\Users\\søren n jessen\\OneDrive\\Dokumenter\\GitHub\\Green-energy-week-9-10\\src\\download.jpg")); // ligger under src som download.jpg en lille fin sol <3 husk backslahes \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
         stage.show();
 
+        gridPane.add(new Text("Ved ikke lige hvad vi skal sætte her, har bare lige pillet lidt rundt"),0,0);
+
+
         gridPane.getChildren().addAll(anchorPane);
+
+        //Her kan du se antal rows og columns
+        gridPane.getColumnCount();
+        gridPane.getRowCount();
+        System.out.println(gridPane.getRowCount());
+        System.out.println(gridPane.getColumnCount());
+
+        //Her kan vi adde rows og columns
+        //gridPane.addColumn();
+        //gridPane.addRow();
 
     }
 
